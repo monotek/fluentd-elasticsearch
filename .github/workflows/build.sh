72ba67dc2bfb5bd7fa04345128983c4c18878d40
André Bauer <andre.bauer@kiwigrid.com>
2019-12-05 14:59:05 +0100

    fix dockerfile

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -9,7 +9,7 @@ DOCKER_REPOSITORY="fluentd-elasticsearch"
 
 echo "Build Docker image with tag / build number ${BUILD_NUMBER} for DockerHubs ${DOCKER_REGISTRY}/${GITHUB_USERNAME}/${DOCKER_REPOSITORY} repo"
 
-echo "${{ secrtes.DOCKER_PASSWORD }}" | docker login -u "${{ secrets.DOCKER_USERNAME }}" --password-stdin
+echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 
 docker build --pull --no-cache -t ${DOCKER_REGISTRY}/${GITHUB_USERNAME}/${DOCKER_REPOSITORY}:latest -t ${DOCKER_REGISTRY}/${GITHUB_USERNAME}/${DOCKER_REPOSITORY}:${BUILD_NUMBER} .
 
