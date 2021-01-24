d0cb351fd27ca347ffaeb1ad59e304314eb03d27
André Bauer <monotek23@gmail.com>
2021-01-24 13:15:06 +0100

    updated fluentd

--- sync-k8s-repo.sh
+++ sync-k8s-repo.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-git remote add kubernetes https://github.com/kubernetes/kubernetes.git
+set -e
+
+if ! git remote | grep -q kubernetes;then
+    echo "kubernetes remote not found... adding it..."
+    git remote add kubernetes https://github.com/kubernetes/kubernetes.git
+fi
+
+echo "fetching kubernetes..."
 git fetch kubernetes --depth=1
+
+echo "checking out kubernetes master..."
 git checkout kubernetes/master cluster/addons/fluentd-elasticsearch/fluentd-es-image/*
 
