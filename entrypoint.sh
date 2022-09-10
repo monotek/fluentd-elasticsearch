60c59380c24038cc96259466fa38c6a84de1a9f7
Robin Ramquist <80628178+robinelastisys@users.noreply.github.com>
2022-09-10 16:33:57 +0200

    fluentd-plugin-opensearch updated (#25)

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,9 +32,14 @@ fi
 # due to between service name and ip address glitch.
 # And this should be needed for downstream helm chart configurations
 # for sniffer_class_name parameter.
-SIMPLE_SNIFFER=$( gem contents fluent-plugin-elasticsearch | grep elasticsearch_simple_sniffer.rb )
-if [ -n "$SIMPLE_SNIFFER" ] && [ -f "$SIMPLE_SNIFFER" ] ; then
-    FLUENTD_ARGS="$FLUENTD_ARGS -r $SIMPLE_SNIFFER"
+SIMPLE_SNIFFER_ELASTICSEARCH=$( gem contents fluent-plugin-elasticsearch | grep elasticsearch_simple_sniffer.rb )
+if [ -n "$SIMPLE_SNIFFER_ELASTICSEARCH" ] && [ -f "$SIMPLE_SNIFFER_ELASTICSEARCH" ] ; then
+    FLUENTD_ARGS="$FLUENTD_ARGS -r $SIMPLE_SNIFFER_ELASTICSEARCH"
+fi
+
+SIMPLE_SNIFFER_OPENSEARCH=$( gem contents fluent-plugin-opensearch | grep opensearch_simple_sniffer.rb )
+if [ -n "$SIMPLE_SNIFFER_OPENSEARCH" ] && [ -f "$SIMPLE_SNIFFER_OPENSEARCH" ] ; then
+    FLUENTD_ARGS="$FLUENTD_ARGS -r $SIMPLE_SNIFFER_OPENSEARCH"
 fi
 
 # Use exec to get the signal
