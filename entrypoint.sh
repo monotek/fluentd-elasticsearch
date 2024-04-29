c3e4ca6b153ae6970a871583faf339d92035789d
André Bauer <andre.bauer@staffbase.com>
2024-04-29 12:44:57 +0200

    lint
    Signed-off-by: André Bauer <andre.bauer@staffbase.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,13 +32,13 @@ fi
 # due to between service name and ip address glitch.
 # And this should be needed for downstream helm chart configurations
 # for sniffer_class_name parameter.
-SIMPLE_SNIFFER_ELASTICSEARCH=$( gem contents fluent-plugin-elasticsearch | grep elasticsearch_simple_sniffer.rb )
-if [ -n "$SIMPLE_SNIFFER_ELASTICSEARCH" ] && [ -f "$SIMPLE_SNIFFER_ELASTICSEARCH" ] ; then
+SIMPLE_SNIFFER_ELASTICSEARCH=$(gem contents fluent-plugin-elasticsearch | grep elasticsearch_simple_sniffer.rb)
+if [ -n "$SIMPLE_SNIFFER_ELASTICSEARCH" ] && [ -f "$SIMPLE_SNIFFER_ELASTICSEARCH" ]; then
     FLUENTD_ARGS="$FLUENTD_ARGS -r $SIMPLE_SNIFFER_ELASTICSEARCH"
 fi
 
-SIMPLE_SNIFFER_OPENSEARCH=$( gem contents fluent-plugin-opensearch | grep opensearch_simple_sniffer.rb )
-if [ -n "$SIMPLE_SNIFFER_OPENSEARCH" ] && [ -f "$SIMPLE_SNIFFER_OPENSEARCH" ] ; then
+SIMPLE_SNIFFER_OPENSEARCH=$(gem contents fluent-plugin-opensearch | grep opensearch_simple_sniffer.rb)
+if [ -n "$SIMPLE_SNIFFER_OPENSEARCH" ] && [ -f "$SIMPLE_SNIFFER_OPENSEARCH" ]; then
     FLUENTD_ARGS="$FLUENTD_ARGS -r $SIMPLE_SNIFFER_OPENSEARCH"
 fi
 
